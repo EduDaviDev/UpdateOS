44132ede4b9265863b0d1e5714b34fd3ff4ba4bc
EduDaviDev <edu.davisouza@gmail.com>
2026-09-10 14:46:19 -0300

    Mod: gitignore: ignore build directory

--- build/dependencies/drivers/vga.d
+++
@@ -1,2 +0,0 @@
-build/kernel/drivers/vga.o: kernel/drivers/vga.c kernel/drivers/vga.h
-kernel/drivers/vga.h:
--- build/dependencies/kernel.d
+++
@@ -1,4 +0,0 @@
-build/kernel/kernel.o: kernel/kernel.c kernel/mboot.h \
- kernel/drivers/vga.h
-kernel/mboot.h:
-kernel/drivers/vga.h:
